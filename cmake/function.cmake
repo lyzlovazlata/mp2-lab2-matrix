38e41553ac361200e4de642ace134d853e8f0d5b
lyzlovazlata <lyzlova.zlata@gmail.com>
2025-10-18 17:34:48 +0300

    Done: Tests, realisation, CMakeLists with functions

---
+++ cmake/function.cmake
@@ -0,0 +1,27 @@
+function(create_project_lib TARGET )
+file(GLOB_RECURSE TARGET_SRC "*.c*")
+file(GLOB_RECURSE TARGET_HD "*.h*")
+add_library(${TARGET} STATIC ${TARGET_SRC} ${TARGET_HD})
+
+get_property ( LIB_LIST GLOBAL PROPERTY LIBS_P)
+list(APPEND LIB_LIST ${TARGET})
+set_property ( GLOBAL PROPERTY LIBS_P ${LIB_LIST})
+
+get_property ( LIB_I_LIST GLOBAL PROPERTY LIBS_I_P)
+list(APPEND LIB_I_LIST ${CMAKE_CURRENT_SOURCE_DIR})
+set_property ( GLOBAL PROPERTY LIBS_I_P ${LIB_ILIST})
+endfunction()
+
+function(create_executable_test_project TARGET) 
+file (GLOB_RECURSE TARGET_SRC "*.c*") 
+file (GLOB_RECURSE TARGET_HD "*.h*")
+add_executable( ${TARGET} ${TARGET_SRC} ${TARGET_HD})
+
+get_property ( LIB_LIST GLOBAL PROPERTY LIBS_P) 
+target_link_libraries(${TARGET} gtest ${LIB_LIST})
+
+get_property ( LIB_I_LIST GLOBAL PROPERTY LIBS_I_P) 
+target_include_directories (${TARGET} PUBLIC ${LIB_I_LIST})
+
+add_test(NAME ${TARGET} COMMAND ${TARGET}) 
+endfunction()
